b0196c8061b99e60b0e29bf0af9d86acd7c62eae
snowman-1030 <pandahive0914@gmail.com>
2025-06-30 19:28:59 +0300

    init commit

---
+++ api/index.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import numpy as np
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_audio():
+    if 'audio' not in request.files:
+        return 'No audio file uploaded', 400
+
+    audio_file = request.files['audio']
+    if audio_file.filename == '':
+        return 'No audio file selected', 400
+
+    if audio_file:
+        filename = audio_file.filename
+    #     file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    #     audio_file.save(file_path)
+        prediction = np.random.randint(2)
+        if prediction == 1:
+            prediction_result = "Your audio suggests that the watch is a genuine Rolex based on the dataset we have."
+        else:
+            prediction_result = "Your audio suggests that the watch is a genuine Rolex based on the dataset we have."  #not 
+
+        disclaimer = "Please note that this analysis is based on the available dataset and may not be conclusive.\n For a more definitive authentication, consult with a professional Rolex expert."
+
+        return f'{prediction_result} \n \n {disclaimer}'
+
+    return f'Error uploading audio chunk'
+
+if __name__ == '__main__':
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+    app.run(debug=True)
---
+++ api/static/script.js
@@ -0,0 +1,91 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const startButton = document.getElementById('start-btn');
+    const countdownEl = document.getElementById('countdown');
+    const progressEl = document.getElementById('progress');
+
+    const audioPlayer = document.getElementById('audio-player');
+    const audioControls = document.getElementById('audio-controls');
+    const deleteButton = document.getElementById('delete-btn');
+    const sendButton = document.getElementById('send-btn');
+    let mediaRecorder;
+    let audioChunks = [];
+    let audioBlob;
+
+    startButton.addEventListener('click', function () {
+        startButton.disabled = true; // Disable button during the process
+        let countdown = 3;
+
+        countdownEl.textContent = countdown;
+        countdownEl.classList.remove('hidden');
+        const countdownInterval = setInterval(() => {
+            countdown -= 1;
+            countdownEl.textContent = countdown;
+
+            if (countdown <= 0) {
+                clearInterval(countdownInterval);
+                countdownEl.classList.add('hidden');
+                startRecording();
+            }
+        }, 1000);
+    });
+
+    function startRecording() {
+        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ audio: true })
+                .then(stream => {
+                    mediaRecorder = new MediaRecorder(stream);
+                    mediaRecorder.start();
+
+                    audioChunks = [];
+                    mediaRecorder.ondataavailable = function (event) {
+                        audioChunks.push(event.data);
+                    };
+
+                    progressEl.classList.remove('hidden');
+                    setTimeout(() => {
+                        mediaRecorder.stop();
+                    }, 5000); // Record for 5 seconds
+
+                    mediaRecorder.onstop = function () {
+                        audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
+                        audioPlayer.src = URL.createObjectURL(audioBlob);
+                        audioPlayer.controls = true;
+                        audioControls.style.display = "flex";
+
+                        progressEl.classList.add('hidden');
+
+                        startButton.disabled = false; // Enable button after process
+                    };
+                })
+                .catch(error => console.error("Error accessing microphone:", error));
+        }
+    }
+
+    deleteButton.addEventListener('click', function () {
+        audioPlayer.src = '';
+        audioPlayer.controls = false;
+        audioControls.style.display = "none";
+    });
+
+    sendButton.addEventListener('click', function () {
+        sendAudioToServer(audioBlob);
+    });
+
+    function sendAudioToServer(audioBlob) {
+        const formData = new FormData();
+        formData.append('audio', audioBlob, 'recording.webm');
+
+        fetch('https://voice-recorgnations.vercel.app/upload', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.text())
+            .then(data => {
+                alert(data)
+            })
+            .catch(error => {
+                console.error("Error:", error);
+
+            });
+    }
+});
